b653610bbdcf658923754d5f4599a73f1666d9bb
Katarzyna Nizioł <niziol.katarzyna25@gmail.com>
2023-10-26 12:43:28 +0200

    add argument in class Product into script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -56,6 +56,8 @@
   class Product {
     constructor(id, data) {
       const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
 
       console.log('new Product:', thisProduct);
     }
